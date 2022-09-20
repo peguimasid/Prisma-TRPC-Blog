8a6f2fac7d0045a5cb785ab8f95c4c55c78aadb2
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-20 19:01:46 -0300

    add HOC to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps, AppType } from 'next/app';
+import { trpc } from '../services/trpc';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: AppType<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default trpc.withTRPC(MyApp);
