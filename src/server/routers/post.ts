5dad10f1d6228f9ba5864143d614fa0d8c53d936
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-30 15:50:25 -0300

    order posts from newest to older

--- src/server/routers/post.ts
+++ src/server/routers/post.ts
@@ -5,6 +5,7 @@ export const postRouter = t.router({
   all: t.procedure.query(async () => {
     const posts = await prisma.post.findMany({
       where: { published: { equals: true } },
+      orderBy: { createdAt: 'desc' },
     });
     return posts.map(({ content, ...rest }) => ({
       content: `${content?.substring(0, 300)}...`,
