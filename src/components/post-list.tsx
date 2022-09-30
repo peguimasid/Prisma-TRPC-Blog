e29e3517d22b21f98312c65668080100e89a34e7
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-30 14:37:17 -0300

    adding styles

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,5 +1,5 @@
 import { trpc } from '@/utils/trpc';
-import { Card, Paragraph } from 'dracula-ui';
+import { Button, Card, List, Paragraph } from 'dracula-ui';
 
 export const PostList = () => {
   const { isLoading, data } = trpc.post.all.useQuery();
@@ -13,13 +13,22 @@ export const PostList = () => {
   }
 
   return (
-    <ul>
+    <List className="w-full max-w-xl">
       {data?.map(({ id, title, content, createdAt }) => (
-        <Card key={id} variant="subtle" color="pink" p="md" m="md">
-          <Paragraph color="blackLight">{title}</Paragraph>
-          <Paragraph color="blackSecondary">{content}</Paragraph>
+        <Card key={id} color="purple" p="md" m="md" className="shadow-none">
+          <Paragraph weight="bold" size="lg" color="blackLight">
+            {title}
+          </Paragraph>
+          <Paragraph size="xs" color="blackSecondary" mt="sm">
+            {content}
+          </Paragraph>
+          <div className="flex w-full justify-end">
+            <Button variant="ghost" color="animated" mt="md" ml="auto">
+              Ver mais
+            </Button>
+          </div>
         </Card>
       ))}
-    </ul>
+    </List>
   );
 };
