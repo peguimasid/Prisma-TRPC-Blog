2bf88ef6cacab2260ed62f79b4d1bd9e34854e31
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-02 13:45:54 -0300

    chore: remove hello router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,9 +1,7 @@
 import { t } from '../trpc';
-import { helloRouter } from './hello';
 import { postRouter } from './post';
 
 export const appRouter = t.router({
-  hello: helloRouter,
   post: postRouter,
 });
 
